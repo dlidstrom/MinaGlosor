54c62968f57cda82780a8981dd90a9e9366a6dae
Daniel Lidstrom <dlidstrom@gmail.com>
2014-01-12 13:52:12 +0100

    Initial commit

---
+++ MinaGlosor.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MinaGlosor.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
