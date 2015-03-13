34f3b2989f65a5a8874f817f161a86920d4b0d7b
Daniel Lidstrom <dlidstrom@gmail.com>
2015-03-13 09:25:28 +0100

    Can detect Arabic text this way

---
+++ MinaGlosor.Test/Api/DetectsCharacterClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace MinaGlosor.Test.Api
+{
+    [TestFixture]
+    public class DetectsCharacterClass
+    {
+        [Test]
+        public void IsArabic()
+        {
+            // Assert
+            const string Sentence = "!چه مى گويى؟ تعريف كن";
+            Assert.That(Sentence, Is.StringMatching(@"\p{IsArabic}"));
+        }
+    }
+}
