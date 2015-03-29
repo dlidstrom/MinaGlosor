a9fe9365d0dcd22b0288579a4e82d86ce3d3ccac
Daniel Lidstrom <dlidstrom@gmail.com>
2015-03-29 21:35:05 +0200

    Fixes #57

---
+++ MinaGlosor.Web/wwwroot/app/shared/progressDirective.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mgApp').directive('progress', Progress);
+
+    function Progress() {
+        return {
+            restrict: 'E',
+            scope: {
+                green: "@green",
+                yellow: "@yellow",
+                done: "@done"
+            },
+            replace: true,
+            template: '<div class="progress progress-flat">'
+                + '<div class="progress-bar progress-bar-success" style="{{\'width: \' + green + \'%\'}}"></div>'
+                + '<div class="progress-bar progress-bar-warning" style="{{\'width: \' + yellow + \'%\'}}"></div>'
+                + '<span class="progress-bar-label">{{done + \'%\'}}</span>'
+                + '</div>'
+        };
+    }
+})();
