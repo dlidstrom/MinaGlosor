d78e175a2bbf11c9f4dcce1dfa49b8a525e18466
Daniel Lidstrom <dlidstrom@gmail.com>
2015-04-12 13:10:41 +0200

    Fixes #46

--- MinaGlosor.Web/wwwroot/app/app.js
+++ MinaGlosor.Web/wwwroot/app/app.js
@@ -2,11 +2,15 @@
     'use strict';
 
     var appVersion = $('meta[name="appVersion"]').attr('content');
-    angular.module('mgApp', ['ngRoute', 'ngMessages', 'toaster']).config(Config).value('AppVersion', appVersion);
+    angular.module('mgApp', ['ngRoute', 'ngMessages', 'toaster'])
+        .value('AppVersion', appVersion)
+        .config(Config);
 
-    Config.$inject = ['$httpProvider'];
+    Config.$inject = ['$httpProvider', '$compileProvider'];
 
-    function Config($httpProvider) {
+    function Config($httpProvider, $compileProvider) {
         $httpProvider.interceptors.push('CustomHttpInterceptor');
+        var isDebuggingEnabled = $('meta[name="isDebuggingEnabled"]').attr('content') === 'true';
+        $compileProvider.debugInfoEnabled(isDebuggingEnabled);
     }
 })();
