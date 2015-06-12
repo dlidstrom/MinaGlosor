5ddb5d5600ca08228140b7ddc315776826a33ad0
Daniel Lidström <dlidstrom@gmail.com>
2015-06-12 13:43:41 +0200

    Show highlights in UI

--- MinaGlosor.Web/wwwroot/app/search/searchIndexController.js
+++ MinaGlosor.Web/wwwroot/app/search/searchIndexController.js
@@ -3,20 +3,33 @@
 
     angular.module('mgApp').controller('SearchIndexController', SearchIndexController);
 
-    SearchIndexController.$inject = ['$location', 'SearchService', 'q', 'result'];
-    function SearchIndexController($location, searchService, q, result) {
+    SearchIndexController.$inject = ['$location', '$sce', 'SearchService', 'q', 'result'];
+    function SearchIndexController($location, $sce, searchService, q, result) {
         var searchIndex = this;
 
         searchIndex.searchService = searchService;
         searchIndex.q = q;
         searchIndex.submit = submit;
-        searchIndex.words = result.words;
+        searchIndex.words = trustHtml(result.words);
 
         function submit(newQ) {
             searchService.search(newQ).then(function (newResult) {
-                searchIndex.words = newResult.words;
+                searchIndex.words = trustHtml(newResult.words);
                 $location.search('q', newQ);
             });
         };
+
+        function trustHtml(words) {
+            var trusted = words.map(function (w) {
+                var text = $sce.trustAsHtml(w.text);
+                var definition = $sce.trustAsHtml(w.definition);
+                return {
+                    text: text,
+                    definition: definition
+                };
+            });
+
+            return trusted;
+        }
     }
 })();
