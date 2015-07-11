43953bf5fb0a71e1c24d21e9ce54229a8519350a
Daniel Lidstrom <dlidstrom@gmail.com>
2015-07-11 13:57:46 +0200

    Close navbar on search

---
+++ MinaGlosor.Web/wwwroot/app/shared/closeNavbarDirective.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mgApp').directive('closeNavbar', CloseNavbar);
+
+    function CloseNavbar() {
+        return {
+            restrict: 'A',
+            link: function (scope, elem) {
+                angular.element(elem).on('submit', function () {
+                    angular.element('#navbar-collapse').collapse('hide');
+                });
+            }
+        };
+    }
+})();
