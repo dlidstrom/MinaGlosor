05a5f3542e768c14a087898440b740781cb86548
Daniel Lidstrom <dlidstrom@gmail.com>
2015-07-15 19:40:31 +0200

    Prevent double-submit

---
+++ MinaGlosor.Web/wwwroot/app/shared/clickOnce.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+    angular.module('mgApp').directive('clickOnce', ClickOnce);
+    ClickOnce.$inject = ['$timeout'];
+    function ClickOnce($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var replacementText = attrs.clickOnce;
+
+                element.bind('click', function () {
+                    $timeout(function () {
+                        if (replacementText) {
+                            element.html(replacementText);
+                        }
+                        element.attr('disabled', true);
+                    });
+                });
+            }
+        };
+    }
+})();
