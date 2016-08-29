7555bf3163afe34e0157bcff1399069ef642a905
Daniel Lidstrom <dlidstrom@gmail.com>
2016-08-29 16:41:19 +0200

    Removed unused classes

--- MinaGlosor.Web/Models/Indexes/WordListIndex.cs
+++ MinaGlosor.Web/Models/Indexes/WordListIndex.cs
@@ -3,55 +3,18 @@ using Raven.Client.Indexes;
 
 namespace MinaGlosor.Web.Models.Indexes
 {
-    // todo remove through admin command?
-    public class WordListIndex : AbstractMultiMapIndexCreationTask<WordListIndex.Result>
+    public class WordListIndex : AbstractIndexCreationTask<WordList>
     {
         public WordListIndex()
         {
-            AddMap<WordList>(wordLists => from wordList in wordLists
-                                          select new
-                                              {
-                                                  wordList.Id,
-                                                  wordList.OwnerId,
-                                                  wordList.Name,
-                                                  wordList.NumberOfWords,
-                                                  NumberOfWordScores = 0
-                                              });
-
-            AddMap<WordScore>(scores => from wordScore in scores
-                                        select new
-                                            {
-                                                Id = wordScore.WordListId,
-                                                wordScore.OwnerId,
-                                                Name = (string)null,
-                                                NumberOfWords = 0,
-                                                NumberOfWordScores = 1
-                                            });
-
-            Reduce = results => from result in results
-                                group result by new { result.Id, result.OwnerId }
-                                    into g
-                                    select new Result
-                                        {
-                                            Id = g.Key.Id,
-                                            OwnerId = g.Key.OwnerId,
-                                            Name = g.First(x => x.Name != null).Name,
-                                            NumberOfWords = g.Sum(x => x.NumberOfWords),
-                                            NumberOfWordScores = g.Sum(x => x.NumberOfWordScores)
-                                        };
-        }
-
-        public class Result
-        {
-            public string Id { get; set; }
-
-            public string Name { get; set; }
-
-            public string OwnerId { get; set; }
-
-            public int NumberOfWords { get; set; }
-
-            public int NumberOfWordScores { get; set; }
+            Map = wordLists => from wordList in wordLists
+                               select new
+                               {
+                                   wordList.Id,
+                                   wordList.OwnerId,
+                                   wordList.Name,
+                                   wordList.NumberOfWords
+                               };
         }
     }
 }
