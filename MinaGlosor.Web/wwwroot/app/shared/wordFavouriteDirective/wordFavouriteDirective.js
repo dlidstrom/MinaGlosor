5364ca7d72b892b91a12e63e648322f69205abc2
Daniel Lidstrom <dlidstrom@gmail.com>
2016-09-16 07:21:52 +0200

    Reorganized directives

---
+++ MinaGlosor.Web/wwwroot/app/shared/wordFavouriteDirective/wordFavouriteDirective.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mgApp')
+        .directive('wordFavourite', WordFavouriteDirective)
+        .controller('WordFavouriteDirectiveController', WordFavouriteDirectiveController);
+
+    function WordFavouriteDirective() {
+        var template
+            = '<span class="favourite">'
+            + '  <i class="glyphicon glyphicon-star action" ng-class="{\'mg-spin\': inProgress, \'glyphicon-star\': isFavourite, \'glyphicon-star-empty\': !isFavourite,  yes: isFavourite, no: !isFavourite}" ng-click="toggle()"></i>'
+            + '</span>';
+        return {
+            restrict: 'E',
+            replace: true,
+            template: template,
+            controller: 'WordFavouriteDirectiveController',
+            scope: {
+                wordId: '=',
+                isFavourite: '='
+            }
+        };
+    }
+
+    WordFavouriteDirectiveController.$inject = ['$scope', 'WordFavouriteService'];
+    function WordFavouriteDirectiveController($scope, wordFavouriteService) {
+        var wordFavourite = $scope;
+
+        wordFavourite.inProgress = false;
+        wordFavourite.isFavourite = $scope.isFavourite;
+        wordFavourite.toggle = toggle;
+
+        function toggle() {
+            wordFavourite.inProgress = true;
+            wordFavouriteService.submit($scope.wordId, !wordFavourite.isFavourite).success(function (response) {
+                wordFavourite.isFavourite = response.isFavourite;
+            }).finally(function () {
+                wordFavourite.inProgress = false;
+            });
+        }
+    }
+})();
