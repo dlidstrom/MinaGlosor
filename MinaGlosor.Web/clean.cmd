6197aaae4d1ff3340c994c3fa636bdd9cc3da1c4
Daniel Lidstrom <dlidstrom@gmail.com>
2016-11-04 06:37:18 +0100

    Fixes #157

--- MinaGlosor.Web/clean.cmd
+++ MinaGlosor.Web/clean.cmd
@@ -1,2 +1,3 @@
 if exist bin\MinaGlosor.Web.dll del /F /S /Q bin\MinaGlosor.Web.dll
 if exist obj\%1\MinaGlosor.Web.dll del /F /S /Q obj\%1\MinaGlosor.Web.dll
+if exist cassette-cache FOR /D %%p IN ("cassette-cache\*.*") DO rmdir "%%p" /s /q
