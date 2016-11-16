e45aaf477bedfb10b7b8006724ce81a250cb78d4
Daniel Lidstrom <dlidstrom@gmail.com>
2016-11-16 15:13:30 +0100

    Progress bars look nice again

--- MinaGlosor.Web/wwwroot/app/pages/progress/progressItem/progressItem.js
+++ MinaGlosor.Web/wwwroot/app/pages/progress/progressItem/progressItem.js
@@ -8,21 +8,6 @@
                 bindings: {
                     item: '<'
                 },
-                controller: 'ProgressItemController',
                 templateUrl: '/wwwroot/app/pages/progress/progressItem/progressItem.html'
-            })
-        .controller('ProgressItemController', ProgressItemController);
-
-    function ProgressItemController() {
-        var $ctrl = this;
-
-        $ctrl.getDoneStyle = getDoneStyle;
-
-        function getDoneStyle(item) {
-            return {
-                width: item.percentDone + '%',
-                'min-width': '3rem'
-            };
-        }
-    }
+            });
 })();
---
+++ MinaGlosor.Web/wwwroot/app/pages/progress/progressItem/progressItemProgress/progressItemProgress.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('pages.progress')
+        .component(
+            'progressItemProgress',
+            {
+                bindings: {
+                    item: '<'
+                },
+                controller: 'ProgressItemProgressController',
+                templateUrl: '/wwwroot/app/pages/progress/progressItem/progressItemProgress/progressItemProgress.html'
+            })
+        .controller('ProgressItemProgressController', ProgressItemProgressController);
+
+    function ProgressItemProgressController() {
+        var $ctrl = this;
+
+        $ctrl.getDoneStyle = getDoneStyle;
+
+        function getDoneStyle(item) {
+            return {
+                width: item.percentDone + '%',
+                'min-width': '3rem'
+            };
+        }
+    }
+})();
